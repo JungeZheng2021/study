b7ef697ddb3ed4802f77be2b7cb9d3c9c5be0e98
milla <hongguanghu@aimsphm.com>
2020-11-14 16:58:19 +0800

    mybatis plus 自定义代码生成配置

---
+++ mybatis-plus-generator/src/test/java/com/study/generator/GeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.generator;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
